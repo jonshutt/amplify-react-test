1e0eb028e929052621f837c8cbd3f166cff802b9
jon shutt <hello@jonshutt.com>
2023-04-26 14:45:55 +0100

    test update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          jons amplify test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>jons amplify test. updated 26th april</p>
       </header>
     </div>
   );
