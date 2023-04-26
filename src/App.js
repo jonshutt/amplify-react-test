cdd8649aeceec03152980b93b2f34384505892b6
jon shutt <hello@jonshutt.com>
2023-04-26 15:11:27 +0100

    another update .....

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>jons amplify test. updated 26th april</p>
+        <p>jons amplify test. updated 26th april....</p>
       </header>
     </div>
   );
