cb8b5ba191f1a07f458affc4a21e93384adea27c
jon shutt <hello@jonshutt.com>
2023-04-26 16:21:57 +0100

    add auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
 
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  // Heading,
+  // Image,
+  // View,
+  // Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
   return (
     <div className="App">
       <header className="App-header">
         <p>jons amplify test. updated 26th april....</p>
+        <Button onClick={signOut}>Sign Out</Button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+// export default App;
